08f06d920374cd71d4fe33a941d84eb835f3eead
Aiham Jassar <165441373+aihamjassar@users.noreply.github.com>
2026-09-13 23:52:20 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,12 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-
+/*
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
+*/
+export default app;
