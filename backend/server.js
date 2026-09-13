a66ae085cbe8285dc6cc1f226e43dd84d6c2bae9
Aiham Jassar <165441373+aihamjassar@users.noreply.github.com>
2026-09-14 00:14:33 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/movie", protectRoute, movieRouter);
 app.use("/api/v1/tv", protectRoute, tvRouter);
 app.use("/api/v1/search", protectRoute, searchRouter);
-/*
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-*/
+/*
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
@@ -39,3 +39,5 @@ app.listen(PORT, () => {
   connectDB();
 });
 
+*/
+export default app;
