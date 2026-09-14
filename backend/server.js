df405c13db96ec14a1bc9ff34ffd9c4472ad4951
Aiham Jassar <165441373+aihamjassar@users.noreply.github.com>
2026-09-14 22:56:54 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-/*
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
@@ -39,5 +39,4 @@ app.listen(PORT, () => {
   connectDB();
 });
 
-*/
-export default app;
+// export default app;
